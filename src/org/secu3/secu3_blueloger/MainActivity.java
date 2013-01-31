4d381064029f13e825c7a387624e2f25b3a4503f
denami <denami@list.ru>
2013-01-31 18:32:38 +0300

    Add language RU Add try initialize bluetooth devise

--- src/org/secu3/secu3_blueloger/MainActivity.java
+++ src/org/secu3/secu3_blueloger/MainActivity.java
@@ -1,15 +1,53 @@
 package org.secu3.secu3_blueloger;
 
+import java.util.UUID;
 import android.os.Bundle;
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	Button btnConnect;  
+	TextView txtLod;
+	private BluetoothAdapter btAdapter = null;  
+	private BluetoothSocket btSocket = null;
+	
+	// SPP UUID service
+	private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+	private static final String TAG = null;
+	
+	// MAC-address of Bluetooth module (you must edit this line)
+	private static String address = "00:15:FF:F2:19:5F";
+	
+	
+	
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		btnConnect = (Button) findViewById(R.id.buttonconnect);
+		txtLod = (TextView) findViewById(R.id.textViewLog);
+		txtLod.setText(R.string.onStartMessage);
+		btAdapter = BluetoothAdapter.getDefaultAdapter();		// get Bluetooth adapter
+		checkBTState();
 	}
 
 	@Override
@@ -18,5 +56,26 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
+	
+	private void checkBTState() {
+	    // Check for Bluetooth support and then check to make sure it is turned on
+	    // Emulator doesn't support Bluetooth and will return null
+	    if(btAdapter==null) { 
+	      errorExit("Fatal Error", "Bluetooth not support");
+	    } else {
+	      if (btAdapter.isEnabled()) {
+	        Log.d(TAG, "...Bluetooth ON...");
+	      } else {
+	        //Prompt user to turn on Bluetooth
+	        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+	        startActivityForResult(enableBtIntent, 1);
+	      }
+	    }
+	  }
+	  private void errorExit(String title, String message){
+		    Toast.makeText(getBaseContext(), title + " - " + message, Toast.LENGTH_LONG).show();
+		    finish();
+	  }
+	  
 
 }
